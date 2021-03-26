b45c4184a98dae2a35b5eee8423f7d4ea4cede1c
deltagare <huiyi.skarner@gmail.com>
2021-03-26 16:40:53 +0100

    Buttons for plus and minus

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello Huyi</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged && <h3>Valuable information I shouldn't see</h3> }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,48 +4,55 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
 import {createStore} from 'redux'; //step 1, create globalized state
+import rootReducer from './reducers' //when it is index.js, don't have to specify the name of the file
+import {Provider} from 'react-redux';
+
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 
-//ACTION - increment, decrement
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  }  
-}
+// //ACTION - increment, decrement
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT',
+//   }  
+// }
 
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  }  
-}
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT',
+//   }  
+// }
 
-//REDUCER
-const counter = (state=0, action) => {
-  switch (action.type) {
-    case 'INCREMENT': 
-        return state + 1;
-    case 'DECREMENT':
-        return state - 1;
-    default: return state;
-  }
-}
+// //REDUCER
+// const counter = (state=0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT': 
+//         return state + 1;
+//     case 'DECREMENT':
+//         return state - 1;
+//     default: return state;
+//   }
+// }
 
-//STORE - Globalized state
-let store = createStore(counter); // takes in a reducer as parameter
+// //STORE - Globalized state
+// let store = createStore(counter); // takes in a reducer as parameter
 
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
+// //Display it in the console
+// store.subscribe(() => console.log(store.getState()));
 
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+// //DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
